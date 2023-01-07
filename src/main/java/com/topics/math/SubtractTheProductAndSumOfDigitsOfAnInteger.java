ca264d0b94e7e817b36609211a082a1c51ff0803
svdyakin <svdyakin@domclick.ru>
2023-01-07 23:00:29 +0300

    add 1281. Subtract the Product and Sum of Digits of an Integer

---
+++ src/main/java/com/topics/math/SubtractTheProductAndSumOfDigitsOfAnInteger.java
@@ -0,0 +1,45 @@
+package com.topics.math;
+
+/*
+Given an integer number n, return the difference between the product of its digits and the sum of its digits.
+
+Example 1:
+Input: n = 234
+Output: 15
+Explanation:
+Product of digits = 2 * 3 * 4 = 24
+Sum of digits = 2 + 3 + 4 = 9
+Result = 24 - 9 = 15
+
+Example 2:
+Input: n = 4421
+Output: 21
+Explanation:
+Product of digits = 4 * 4 * 2 * 1 = 32
+Sum of digits = 4 + 4 + 2 + 1 = 11
+Result = 32 - 11 = 21
+
+Constraints: 1 <= n <= 10^5
+*/
+
+public class SubtractTheProductAndSumOfDigitsOfAnInteger {
+    public int subtractProductAndSum(int n) {
+        int digProduct = 1;
+        int digSum = 0;
+        int tempt;
+
+        while (n > 0) {
+            tempt = n % 10;
+            digProduct *= tempt;
+            digSum += tempt;
+
+            n /= 10;
+        }
+        return digProduct - digSum;
+    }
+
+    public static void main(String[] args) {
+        SubtractTheProductAndSumOfDigitsOfAnInteger calculator = new SubtractTheProductAndSumOfDigitsOfAnInteger();
+        System.out.println(calculator.subtractProductAndSum(4421));
+    }
+}
